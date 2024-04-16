f3773a2001cbd6769939ae5d056b57806b707df0
Satchel Grant <grantsrb@stanford.edu>
2024-04-15 21:33:48 -0700

    enable concatenating data for long context streams, add mask options

--- datas.py
+++ datas.py
@@ -1,5 +1,6 @@
 import pickle
 import dl_utils.tokenizer
+from dl_utils.utils import rolling_window
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -9,57 +10,168 @@ import os
 class CausalDataset(torch.utils.data.Dataset):
     def __init__(self,
                  data,
+                 labels=None,
+                 masks=None,
                  pad_id=0,
                  bos_id=1,
                  eos_id=2,
+                 concat=False,
+                 cat_seq_len=None,
                  *args, **kwargs):
         """
         Args:
             data: str or list or ndarray or torch LongTensor
                 if string, the path to the data. Otherwise, 
-                should be a number of sequences that are all the
-                same length and already tokenized in id form.
+                should be a number of sequences that are already
+                tokenized in id form. If concat is false, all of the
+                sequences should be the same length.
+            labels: None or same as data
+                optionally argue labels in addition to the data.
+                These will be used as the output sequences. This argument
+                should be a number of sequences that are already
+                tokenized in id form. If concat is false, all of the
+                sequences should be the same length.
+            masks: None or same as data or dict
+                optionally boolean masks. If a dict is argued, will assume
+                that all values are masks of the same shape and type as
+                data. Do not argue padding masks.
             pad_id: int
                 the padding id
+            bos_id: int
+                the beginning of sequence id
+            eos_id: int
+                the end of sequence id
+            concat: bool
+                if true, will concat all samples together into one long
+                sequence that will then be sampled from uniformly.
+            cat_seq_len: None or int
+                if concatenate is true, the data will be sampled in
+                sequence lengths of cat_seq_len.
         """
         self.pad_id = pad_id
         self.bos_id = bos_id
         self.eos_id = eos_id
+        self.seq_len = cat_seq_len
+        self.concat = concat
+
         if type(data)==str:
             data_path = data
             if ".p" in data_path:
                 with open(self.data_path, "rb") as f:
                     data = pickle.load(f)
-        self.id_seqs = data
-        if type(self.id_seqs)==type(np.zeros((1,))):
-            self.id_seqs = torch.LongTensor(self.id_seqs)
-        elif type(self.id_seqs)==list:
-            self.id_seqs = torch.LongTensor(self.id_seqs)
-        self.id_seqs = self.id_seqs.long()
+            if labels:
+                data_path = labels
+                if ".p" in data_path:
+                    with open(self.data_path, "rb") as f:
+                        labels = pickle.load(f)
+            if masks:
+                if type(masks)==str:
+                    data_path = masks
+                    if ".p" in data_path:
+                        with open(self.data_path, "rb") as f:
+                            masks = pickle.load(f)
+                elif type(masks)==dict:
+                    for k in masks:
+                        data_path = masks[k]
+                        if ".p" in data_path:
+                            with open(self.data_path, "rb") as f:
+                                masks[k] = pickle.load(f)
+        self.inpt_seqs = data
+        self.outp_seqs = labels
+        self.masks = masks
+        if self.masks is not None and type(self.masks)!=dict:
+            self.masks = { "mask": self.masks }
+
+        if self.seq_len is None:
+            if self.concat:
+                self.seq_len = 3*len(self.inpt_seqs[0])
+            else:
+                self.seq_len = len(self.inpt_seqs[0])
+        if hasattr(self.inpt_seqs, "shape") and self.concat:
+            B,S = self.inpt_seqs.shape
+            self.inpt_seqs = self.inpt_seqs.reshape(B*S,-1).squeeze()
+            if self.outp_seqs is not None:
+                self.outp_seqs = self.outp_seqs.reshape(B*S,-1).squeeze()
+            if self.masks is not None:
+                for k in self.masks:
+                    self.masks[k] = self.masks[k].reshape(B*S,-1).squeeze()
+        if type(self.inpt_seqs)==type(np.zeros((1,))):
+            self.inpt_seqs = torch.LongTensor(self.inpt_seqs)
+            if self.outp_seqs is not None:
+                self.outp_seqs = torch.LongTensor(self.outp_seqs)
+            if self.masks is not None:
+                for k in self.masks:
+                    self.masks[k] = torch.BoolTensor(self.masks[k])
+        elif type(self.inpt_seqs)==list and self.concat:
+            seqs = []
+            for seq in self.inpt_seqs: seqs += seq
+            self.inpt_seqs = torch.LongTensor(seqs)
+            if self.outp_seqs is not None:
+                seqs = []
+                for seq in self.outp_seqs: seqs += seq
+                self.outp_seqs = torch.LongTensor(seqs)
+            if self.masks is not None:
+                for k in self.masks:
+                    seqs = []
+                    for seq in self.masks[k]: seqs += seq
+                    self.masks[k] = torch.BoolTensor(seqs)
+        else:
+            self.inpt_seqs = torch.LongTensor(self.inpt_seqs)
+            if self.outp_seqs is not None:
+                self.outp_seqs = torch.LongTensor(self.outp_seqs)
+            if self.masks is not None:
+                for k in self.masks:
+                    self.masks[k] = torch.BoolTensor(self.masks[k])
 
     def __len__(self):
-        return len(self.id_seqs)
+        if self.concat: return len(self.inpt_seqs)-self.seq_len-1
+        return len(self.inpt_seqs)
 
     def __getitem__(self, idx):
         """
         Returns:
-            ids: torch LongTensor (S,)
-                the integer ids of the tokens
-            step_mask: torch LongTensor (S,)
-                integers representing which environment step the
-                language belongs to.
+            ret_dict: dict
+                "input_ids": long tensor (S,)
+                "output_ids": long tensor (S,)
+                "input_pad_mask": bool tensor (S,)
+                "output_pad_mask": bool tensor (S,)
         """
-        samp = self.id_seqs[idx]
+        if self.concat:
+            samp = self.inpt_seqs[idx:idx+self.seq_len]
+        else:
+            samp = self.inpt_seqs[idx]
         input_ids = samp[:-1]
-        in_pad_mask = (input_ids==self.pad_id)|(input_ids==self.eos_id)
-        output_ids = samp[1:]
-        out_pad_mask = (output_ids==self.pad_id)|(output_ids==self.bos_id)
-        return {
-            "input_ids": input_ids,
-            "input_pad_mask": in_pad_mask,
-            "output_ids": output_ids,
-            "output_pad_mask": out_pad_mask,
-        }
+
+        ## Outputs
+        if self.outp_seqs is None:
+            output_ids = samp[1:]
+        else:
+            if self.concat:
+                output_ids = self.outp_seqs[idx:idx+self.seq_len]
+            else:
+                output_ids = self.outp_seqs[idx]
+
+        ## Masks
+        masks = dict() if self.masks is None else\
+                {k:v for k,v in self.masks.items()}
+        for k in masks:
+            if self.concat:
+                masks[k] = masks[k][idx:idx+self.seq_len]
+            else:
+                masks[k] = masks[k][idx]
+        if "input_pad_mask" not in masks:
+            m = (input_ids==self.pad_id)|(input_ids==self.eos_id)
+            masks["input_pad_mask"] = m
+        if "output_pad_mask" not in masks:
+            m = (output_ids==self.pad_id)|(output_ids==self.bos_id)
+            masks["output_pad_mask"] = m
+
+        #print("input_ids", input_ids.shape)
+        #print("output_ids", output_ids.shape)
+        #for m in masks:
+        #    print(m, masks[m].shape)
+        #assert False
+        return {"input_ids":input_ids, "output_ids":output_ids, **masks,}
 
 def get_datasets(config):
     """
@@ -109,10 +221,10 @@ def get_datasets(config):
     config["n_tokens"] = seq_offset+seq_max
     config["seq_len"] = samples.shape[-1]
     n_train = int(n_samples*0.8)
-    train_samps = samples[:n_train]
-    val_samps = samples[n_train:]
-    train_dataset = CausalDataset(train_samps, **special_ids)
-    val_dataset =   CausalDataset(val_samps, **special_ids)
+    train_samps =   samples[:n_train]
+    val_samps =     samples[n_train:]
+    train_dataset = CausalDataset(data=train_samps, **special_ids)
+    val_dataset =   CausalDataset(data=val_samps, **special_ids)
 
     # Kinda hacky, but will help with printing example
     ids = np.unique(samples.reshape(-1))
